43a1ded5547cd3ab0689073e3b5fe5f0de54cfdf
Fran <frajarsa@gmail.com>
2023-10-11 10:13:32 -0300

    Table and navbar functionalities in progress

--- project2/src/app/features/students-table/students-table.component.ts
+++ project2/src/app/features/students-table/students-table.component.ts
@@ -5,16 +5,16 @@ import { Student } from 'src/app/interfaces/student';
 
 const datos: Student[] = [
 
-  { id: Math.floor(Math.random() * 10000), nombre: "Juan", apellido: "Perez", email: "juan.perez@example.com", dni: 12345678 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Maria", apellido: "Lopez", email: "maria.lopez@example.com", dni: 87654321 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Carlos", apellido: "Gonzalez", email: "carlos.gonzalez@example.com", dni: 98765432 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Ana", apellido: "Rodriguez", email: "ana.rodriguez@example.com", dni: 10987654 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Jose", apellido: "Garcia", email: "jose.garcia@example.com", dni: 11109876 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Francisco", apellido: "Martinez", email: "francisco.martinez@example.com", dni: 12111098 },
-  { id: Math.floor(Math.random() * 10000), nombre: "David", apellido: "Sanchez", email: "david.sanchez@example.com", dni: 13121110 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Laura", apellido: "Gomez", email: "laura.gomez@example.com", dni: 14131211 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Antonio", apellido: "Hernandez", email: "antonio.hernandez@example.com", dni: 15141312 },
-  { id: Math.floor(Math.random() * 10000), nombre: "Marta", apellido: "Dominguez", email: "marta.dominguez@example.com", dni: 16151413 },
+  { id: 1234, nombre: "Juan", apellido: "Perez", email: "juan.perez@example.com", dni: 12345678 },
+  { id: 5678, nombre: "Maria", apellido: "Lopez", email: "maria.lopez@example.com", dni: 87654321 },
+  { id: 9012, nombre: "Carlos", apellido: "Gonzalez", email: "carlos.gonzalez@example.com", dni: 98765432 },
+  { id: 2345, nombre: "Ana", apellido: "Rodriguez", email: "ana.rodriguez@example.com", dni: 10987654 },
+  { id: 6789, nombre: "Jose", apellido: "Garcia", email: "jose.garcia@example.com", dni: 11109876 },
+  { id: 5432, nombre: "Francisco", apellido: "Martinez", email: "francisco.martinez@example.com", dni: 12111098 },
+  { id: 7890, nombre: "David", apellido: "Sanchez", email: "david.sanchez@example.com", dni: 13121110 },
+  { id: 1432, nombre: "Laura", apellido: "Gomez", email: "laura.gomez@example.com", dni: 14131211 },
+  { id: 9876, nombre: "Antonio", apellido: "Hernandez", email: "antonio.hernandez@example.com", dni: 15141312 },
+  { id: 4321, nombre: "Marta", apellido: "Daguerre", email: "marta.dominguez@example.com", dni: 16151413 },
 
 ];
 
